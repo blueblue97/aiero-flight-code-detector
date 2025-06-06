5dfaff9ce81c0c8dfb5faa2258562764a9d10331
blueblue97 <Lucasjohnson2706@hotmail.com>
2025-06-06 16:59:09 -0400

    Update sky_brain.py

--- sky_brain.py
+++ sky_brain.py
@@ -1,14 +1,18 @@
+import pandas as pd
+
 def detect_conflicts(df):
     conflicts = []
+    df = df.sort_values(by='timestamp')
     for i in range(len(df)):
-        for j in range(i + 1, len(df)):
-            if df.iloc[i]['time'] == df.iloc[j]['time']:
-                dist_lat = abs(df.iloc[i]['latitude'] - df.iloc[j]['latitude'])
-                dist_lon = abs(df.iloc[i]['longitude'] - df.iloc[j]['longitude'])
-                dist_alt = abs(df.iloc[i]['altitude'] - df.iloc[j]['altitude'])
-                if dist_lat < 1 and dist_lon < 1 and dist_alt < 1000:
+        for j in range(i+1, len(df)):
+            if abs(df.iloc[i]['altitude'] - df.iloc[j]['altitude']) < 1000:
+                if abs(df.iloc[i]['lat'] - df.iloc[j]['lat']) < 0.05 and abs(df.iloc[i]['lon'] - df.iloc[j]['lon']) < 0.05:
                     conflicts.append({
-                        "flight_id": df.iloc[i]['flight_id'],
-                        "conflict_with": df.iloc[j]['flight_id']
+                        'Flight 1': df.iloc[i]['flight_id'],
+                        'Flight 2': df.iloc[j]['flight_id'],
+                        'Altitudes': (df.iloc[i]['altitude'], df.iloc[j]['altitude']),
+                        'Latitudes': (df.iloc[i]['lat'], df.iloc[j]['lat']),
+                        'Longitudes': (df.iloc[i]['lon'], df.iloc[j]['lon']),
+                        'Timestamps': (df.iloc[i]['timestamp'], df.iloc[j]['timestamp'])
                     })
-    return conflicts
+    return pd.DataFrame(conflicts)
